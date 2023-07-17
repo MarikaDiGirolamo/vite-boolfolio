22b1718678ca141c2ee70ab3524b68580dd2fdeb
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-07-17 18:11:56 +0200

    Template basic

---
+++ src/components/AppMain.vue
@@ -0,0 +1,19 @@
+<script>
+import axios from "axios";
+export default {
+    name: "AppMain",
+    data() {
+        return {
+            apiUrl: "http://localhost:8000/api/",
+        }
+
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    }
+}
+
+</script>
